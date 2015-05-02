885cf4eacbb4a582b873969917b0ff83e7c67bf4
Dustin Bergman <dustin.bergman@gmail.com>
2015-05-02 15:15:11 -0400

    Added short List Albums vc

---
+++ source/shortList/shortList/SLListAlbumsVC.h
@@ -0,0 +1,13 @@
+//
+//  SLListAlbumsVC.h
+//  shortList
+//
+//  Created by Dustin Bergman on 5/2/15.
+//  Copyright (c) 2015 Dustin Bergman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLListAlbumsVC : URBNModel
+
+@end
---
+++ source/shortList/shortList/SLListAlbumsVC.m
@@ -0,0 +1,13 @@
+//
+//  SLListAlbumsVC.m
+//  shortList
+//
+//  Created by Dustin Bergman on 5/2/15.
+//  Copyright (c) 2015 Dustin Bergman. All rights reserved.
+//
+
+#import "SLListAlbumsVC.h"
+
+@implementation SLListAlbumsVC
+
+@end
