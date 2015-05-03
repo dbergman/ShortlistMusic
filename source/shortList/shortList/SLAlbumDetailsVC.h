d33246a5f9af982ab15207941deeca6f1f9dc3f6
Dustin Bergman <dustin.bergman@gmail.com>
2015-05-03 13:51:47 -0400

    Added Album Details VC

---
+++ source/shortList/shortList/SLAlbumDetailsVC.h
@@ -0,0 +1,13 @@
+//
+//  SLAlbumDetailsVC.h
+//  shortList
+//
+//  Created by Dustin Bergman on 5/3/15.
+//  Copyright (c) 2015 Dustin Bergman. All rights reserved.
+//
+
+#import "SLBaseVC.h"
+
+@interface SLAlbumDetailsVC : SLBaseVC
+
+@end
---
+++ source/shortList/shortList/SLAlbumDetailsVC.m
@@ -0,0 +1,22 @@
+//
+//  SLAlbumDetailsVC.m
+//  shortList
+//
+//  Created by Dustin Bergman on 5/3/15.
+//  Copyright (c) 2015 Dustin Bergman. All rights reserved.
+//
+
+#import "SLAlbumDetailsVC.h"
+
+@interface SLAlbumDetailsVC ()
+
+@end
+
+@implementation SLAlbumDetailsVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+@end
