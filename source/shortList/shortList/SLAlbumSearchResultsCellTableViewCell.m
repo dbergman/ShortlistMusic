76fbe406e35d6f58cfa4bc74150741c7dcf4709a
Dustin Bergman <dustin.bergman@gmail.com>
2015-05-03 13:45:44 -0400

    Added album release year label

--- source/shortList/shortList/SLAlbumSearchResultsCellTableViewCell.m
+++ source/shortList/shortList/SLAlbumSearchResultsCellTableViewCell.m
@@ -17,6 +17,7 @@ static CGFloat const kSLAlbumArtSize = 100.0;
 
 @property (nonatomic, strong) UIImageView *albumArt;
 @property (nonatomic, strong) UILabel *albumNameLabel;
+@property (nonatomic, strong) UILabel *albumReleaseYearLabel;
 
 @end
 
@@ -33,21 +34,31 @@ static CGFloat const kSLAlbumArtSize = 100.0;
         
         self.albumNameLabel = [UILabel new];
         [self.albumNameLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
-        self.albumNameLabel.numberOfLines = 0;
+        self.albumNameLabel.numberOfLines = 3;
         self.albumNameLabel.lineBreakMode = NSLineBreakByWordWrapping;
         self.albumNameLabel.textColor = [UIColor whiteColor];
         self.albumNameLabel.preferredMaxLayoutWidth = self.contentView.frame.size.width - kSLAlbumArtSize;
         [self.contentView addSubview:self.albumNameLabel];
+        
+        self.albumReleaseYearLabel = [UILabel new];
+        [self.albumReleaseYearLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+        self.albumReleaseYearLabel.numberOfLines = 1;
+        self.albumReleaseYearLabel.textColor = [UIColor whiteColor];
+        self.albumReleaseYearLabel.preferredMaxLayoutWidth = self.contentView.frame.size.width - kSLAlbumArtSize;
+        [self.contentView addSubview:self.albumReleaseYearLabel];
 
-        NSDictionary *views = NSDictionaryOfVariableBindings(_albumArt, _albumNameLabel);
+        NSDictionary *views = NSDictionaryOfVariableBindings(_albumArt, _albumNameLabel, _albumReleaseYearLabel);
         NSDictionary *metrics = @{@"albumArtSize":@(kSLAlbumArtSize), @"smallMargin":@(MarginSizes.small)};
         
         [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-smallMargin-[_albumArt(albumArtSize)]-smallMargin-[_albumNameLabel]" options:0 metrics:metrics views:views]];
         
-        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-smallMargin-[_albumNameLabel]" options:0 metrics:metrics views:views]];
-        
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-smallMargin-[_albumArt(albumArtSize)]-smallMargin-[_albumReleaseYearLabel]" options:0 metrics:metrics views:views]];
         
         [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.albumArt attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.contentView attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:1.0]];
+        
+        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.albumNameLabel attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.albumArt attribute:NSLayoutAttributeTop multiplier:1.0 constant:1.0]];
+        
+        [self.contentView addConstraint:[NSLayoutConstraint constraintWithItem:self.albumReleaseYearLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.albumArt attribute:NSLayoutAttributeBottom multiplier:1.0 constant:1.0]];
     }
     
     return self;
@@ -56,11 +67,7 @@ static CGFloat const kSLAlbumArtSize = 100.0;
 - (void)configCellWithItunesAlbum:(ItunesAlbum *)album {
     [self.albumArt sd_setImageWithURL:[NSURL URLWithString:album.artworkUrl100] placeholderImage:nil];
     self.albumNameLabel.text = album.collectionName;
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
+    self.albumReleaseYearLabel.text = [NSString stringWithFormat:@"Release Year: %@", album.releaseYear];
 }
 
 @end
