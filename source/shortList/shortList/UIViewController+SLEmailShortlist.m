52199f6f6c199b0291e84d33347aed642a7865ba
Dustin Bergman <dustin.bergman@gmail.com>
2015-08-16 09:41:23 -0400

    Added Category for email sharing

---
+++ source/shortList/shortList/UIViewController+SLEmailShortlist.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+SLEmailShortlist.m
+//  shortList
+//
+//  Created by Dustin Bergman on 8/15/15.
+//  Copyright (c) 2015 Dustin Bergman. All rights reserved.
+//
+
+#import "UIViewController+SLEmailShortlist.h"
+
+@implementation UIViewController (SLEmailShortlist)
+
+@end
