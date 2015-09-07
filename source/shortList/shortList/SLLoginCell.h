bf4eeed34041cc55edef6c800e4c79a2b2ace410
Dustin Bergman <dustin.bergman@gmail.com>
2015-09-06 22:37:00 -0400

    Added login cell to more tab

---
+++ source/shortList/shortList/SLLoginCell.h
@@ -0,0 +1,13 @@
+//
+//  SLLoginCell.h
+//  shortList
+//
+//  Created by Dustin Bergman on 9/6/15.
+//  Copyright © 2015 Dustin Bergman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLLoginCell : UITableViewCell
+
+@end
---
+++ source/shortList/shortList/SLLoginCell.m
@@ -0,0 +1,13 @@
+//
+//  SLLoginCell.m
+//  shortList
+//
+//  Created by Dustin Bergman on 9/6/15.
+//  Copyright © 2015 Dustin Bergman. All rights reserved.
+//
+
+#import "SLLoginCell.h"
+
+@implementation SLLoginCell
+
+@end
