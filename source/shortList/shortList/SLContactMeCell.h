34d38d3d952c0fcfe88579918568271e6081febe
Dustin Bergman <dustin.bergman@gmail.com>
2015-11-24 20:38:55 -0500

    Added contact me cell

---
+++ source/shortList/shortList/SLContactMeCell.h
@@ -0,0 +1,13 @@
+//
+//  SLContactMeCell.h
+//  shortList
+//
+//  Created by Dustin Bergman on 11/24/15.
+//  Copyright © 2015 Dustin Bergman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLContactMeCell : UITableViewCell
+
+@end
---
+++ source/shortList/shortList/SLContactMeCell.m
@@ -0,0 +1,13 @@
+//
+//  SLContactMeCell.m
+//  shortList
+//
+//  Created by Dustin Bergman on 11/24/15.
+//  Copyright © 2015 Dustin Bergman. All rights reserved.
+//
+
+#import "SLContactMeCell.h"
+
+@implementation SLContactMeCell
+
+@end
