4b6f86f5deca35324c83ca55a7c86dbcd3563a3e
Dustin Bergman <dustin.bergman@gmail.com>
2015-11-29 16:26:26 -0500

    Added coredata to parse migration controller

---
+++ source/shortList/shortList/SLShortlistCoreDataMigrtationController.h
@@ -0,0 +1,13 @@
+//
+//  SLShortlistCoreDataMigrtationController.h
+//  shortList
+//
+//  Created by Dustin Bergman on 11/29/15.
+//  Copyright © 2015 Dustin Bergman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLShortlistCoreDataMigrtationController : NSObject
+
+@end
---
+++ source/shortList/shortList/SLShortlistCoreDataMigrtationController.m
@@ -0,0 +1,13 @@
+//
+//  SLShortlistCoreDataMigrtationController.m
+//  shortList
+//
+//  Created by Dustin Bergman on 11/29/15.
+//  Copyright © 2015 Dustin Bergman. All rights reserved.
+//
+
+#import "SLShortlistCoreDataMigrtationController.h"
+
+@implementation SLShortlistCoreDataMigrtationController
+
+@end
