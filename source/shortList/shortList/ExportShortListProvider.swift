8a61ac1d08c7663134075c17a69db4d622b14b76
Dustin Bergman <dustin.bergman@gmail.com>
2022-01-15 14:30:06 -0800

    Fixed Flurry build issue

---
+++ source/shortList/shortList/ExportShortListProvider.swift
@@ -0,0 +1,13 @@
+//
+//  ExportShortListProvider.swift
+//  shortList
+//
+//  Created by Dustin Bergman on 1/15/22.
+//  Copyright © 2022 Dustin Bergman. All rights reserved.
+//
+
+import Foundation
+
+class ExportShortListProvider {
+    
+}
