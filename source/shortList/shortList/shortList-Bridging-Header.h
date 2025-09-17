4d2f0f27e25fd710024d4cc60374da984f2b51c5
Dustin Bergman <dustin.bergman@GMAIL.COM>
2025-09-16 20:55:00 -0700

    Delete the old shit

--- source/shortList/shortList/shortList-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-#import "SLShortlist.h"
-#import "SLShortListAlbum.h"
-#import "SLBaseVC.h"
-#import "SLLoginVC.h"
-#import "SLStyle.h"
-#import "UIViewController+SLToastBanner.h"
